fff6f39ba000f73da8446fefa083623ddc334e28
NeDgEr Five <nedger.five@gmail.com>
2024-12-20 18:18:29 +0200

    feat: проба

--- src/all.js
+++ src/all.js
@@ -4,5 +4,21 @@
  * @returns Promise
  */
 module.exports.all = function all(promisesArray) {
-  throw new Error('Not implemented'); // remove me and write your code
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completed = 0;
+
+    for (let i = 0; i < promisesArray.length; i++) {
+      Promise.resolve(promisesArray[i])
+        .then(value => {
+          results[i] = value;
+          completed++;
+
+          if (completed === promisesArray.length) {
+            resolve(results);
+          }
+        })
+        .catch(reject);
+    }
+  });
 };
